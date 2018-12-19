80a5dfbc2c28c147410a085c59ecb4461462c59e
EC2 Default User <ec2-user@ip-172-31-30-32.us-east-2.compute.internal>
2018-12-19 05:03:50 +0000

    Studio CRUD done.

---
+++ app/controllers/studios_controller.rb
@@ -0,0 +1,47 @@
+class StudiosController < ApplicationController
+  
+  # get /studios
+  def index
+    @studios = Studio.all
+  end
+  
+  # post /studios
+  def create
+    @studio = Studio.new(studio_params)
+    @studio.save
+    redirect_to studios_path
+  end
+  
+  # get /studios/new
+  def new
+    @studio = Studio.new
+  end
+  
+  # get /studios/:id/edit
+  def edit
+    @studio = Studio.find(params[:id])
+  end
+  
+  # get /studios/:id
+  def show
+    @studio = Studio.find(params[:id])
+  end
+  
+  # patch /studios/:id
+  def update
+    @studio = Studio.find(params[:id])
+    @studio.update_attributes(studio_params)
+    @studio.save
+    redirect_to studio_path(@studio)
+  end
+  
+  # delete /studios/:id
+  def destroy
+    Studio.find(params[:id]).destroy
+    redirect_to studios_path
+  end
+  
+  def studio_params
+    params.require(:studio).permit(:name,:address)
+  end
+end
