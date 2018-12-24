72030402370051f33e28de6abfbe69917d4edfb1
EC2 Default User <ec2-user@ip-172-31-30-32.us-east-2.compute.internal>
2018-12-24 14:39:57 +0000

    add remember-me

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,17 +4,40 @@ module SessionsHelper
   end
   
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
   
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    #セッションにあればそこから取り出す
+    if user_id = session[:user_id]
+      @current_user ||= User.find_by(id: user_id)
+    #セッションになければクッキーからユーザーを取り出し、対応するセッションにログインする
+    elsif user_id = cookies.signed[:user_id]
+      user ||= User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    #どこにもなければnilを返す
     end
   end
   
   def logged_in?
     !current_user.nil?
+  end
+  
+  #cookie
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:token] = user.remember_token
+  end
+  
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,36 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save {email.downcase! }
   validates :name, presence: true
   validates :email, presence: true, uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true
+  
+  
+  #トークンを生成するメソッド
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  #引数の文字列のダイジェストを返すメソッド
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
+  #引数のトークンがremember_digestとあっているか？を認証するメソッド
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
