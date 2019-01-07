0e274cbbb48c617e25ea113986fe8f4c6b111d5a
EC2 Default User <ec2-user@ip-172-31-30-32.us-east-2.compute.internal>
2019-01-07 09:07:25 +0000

    #24

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_07_013849) do
+ActiveRecord::Schema.define(version: 2019_01_07_082234) do
 
   create_table "areas", force: :cascade do |t|
     t.string "name"
@@ -59,12 +59,12 @@ ActiveRecord::Schema.define(version: 2019_01_07_013849) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "area_id"
-    t.string "image"
     t.string "telno"
     t.string "url"
     t.integer "created_user_id"
     t.float "latitude"
     t.float "longitude"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,20 +14,25 @@ User.create!(
               email:"foo@bar",
               password:"foobar")
 
-30.times do |n|
-  name = "スタジオ" + Takarabako.open
-  area_id = Random.rand(1..4)
-  Studio.create!(
-                name: name,
-                area_id: area_id,
-                image: "default-studio-icon.jpeg",
-                address: "東京都新宿区西新宿２丁目８−１",
-                telno: "000-0000-0000",
-                url: "http://www.testtest.com")
-end
-
-=begin
-uri = URI.parse URI.encode "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&input=牛角&language=ja&fields=id&key=#{ENV['GOOGLEMAPS_IP_KEY']}"
+uri = URI.parse URI.encode "https://maps.googleapis.com/maps/api/place/textsearch/json?query=音楽スタジオ+新宿駅&language=ja&key=#{ENV['GOOGLEMAPS_IP_KEY']}"
 res = HTTP.get(uri).to_s
 response = JSON.parse(res)
-=end
+response['results'].each do |result|
+  
+  studio = Studio.new(
+                name: result['name'],
+                area_id: 1,
+                address: result['formatted_address'].slice(13..-1),
+                latitude: result['geometry']['location']['lat'],
+                longitude: result['geometry']['location']['lng'],
+                telno: "000-0000-0000",
+                url: "http://www.testtest.com",
+                )
+  if result.has_key?("photos")
+    photo_reference = result['photos'][0]['photo_reference'] 
+    url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxheight=400&photoreference=#{photo_reference}&key=#{ENV['GOOGLEMAPS_IP_KEY']}"
+    studio.remote_image_url = url
+  end
+  studio.save!
+  
+end
