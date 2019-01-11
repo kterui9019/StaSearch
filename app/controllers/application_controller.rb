5e6f196dfbac0c36bff028cba201ed22aef759dc
EC2 Default User <ec2-user@ip-172-31-30-32.us-east-2.compute.internal>
2019-01-11 02:03:53 +0000

    #48

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :set_search_query
   
   def set_search_query
-    @hash_tags = HashTag.all
+    @q_hash_tags = HashTag.all
     words = params[:q].delete(:name_or_address_cont) if params[:q].present?
     if words.present?
       params[:q][:groupings] = []
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
       end
     end
     @q = Studio.ransack(params[:q])
-    @studios = @q.result(distinct: true).includes(:hash_tags, :hash_tag_relationships, :reviews).paginate(page: params[:page], per_page: 9)
+    @q_studios = @q.result(distinct: true).includes(:hash_tags, :hash_tag_relationships, :reviews).paginate(page: params[:page], per_page: 9)
     
   end
   
